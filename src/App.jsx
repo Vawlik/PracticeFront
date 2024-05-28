97b1b4c987f8a2daf2ce3171f72651a89cb3352c
Даниил Виноходов <strelokst.strelokst@yandex.ru>
2024-05-28 20:49:03 +0300

    Выполнено задание по добавлению Ленивой подгрузки роутов с помощью React.lazy и Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu';
-import Home from './components/Home';
-import AboutUs from './components/AboutUs';
+
+const Home = lazy(() => import('./components/Home'));
+const AboutUs = lazy(() => import('./components/AboutUs'));
 
 const App = () => {
     return (
         <Router>
             <div>
                 <Menu />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<AboutUs />} />
-                    <Route path="/about/:id" element={<AboutUs />} />
-                </Routes>
+                <Suspense fallback={<div>Загрузка...</div>}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<AboutUs />} />
+                        <Route path="/about/:id" element={<AboutUs />} />
+                    </Routes>
+                </Suspense>
             </div>
         </Router>
     );
